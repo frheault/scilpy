Fix silently wrong fixel/angular-error/mean-fixel results for non-RAS input by rebinding streamlines to the reoriented image grid

src/scilpy/cli/scil_bundle_fixel_analysis.py
@@ -96,13 +96,15 @@ The script produces various output:
 """
 
 import argparse
-import os
-
-import nibabel as nib
-import numpy as np
 import logging
+import os
 from pathlib import Path
 
+import numpy as np
+from dipy.io.streamline import load_tractogram
+
+from scilpy.io.stateful_image import StatefulImage
+from scilpy.io.streamlines import rebind_sft_to_simg
 from scilpy.io.utils import (add_overwrite_arg, add_processes_arg,
                              assert_headers_compatible, assert_inputs_exist,
                              add_verbose_arg,
@@ -157,7 +159,7 @@ def _build_arg_parser():
                          'this value will pass the relative threshold test. '
                          '\nMust be between 0 and 1 [%(default)s].')
 
-    g1.add_argument('--norm', default='voxel', nargs='+', type=str,
+    g1.add_argument('--norm', default=['voxel'], nargs='+', type=str,
                     choices=['fixel', 'voxel', 'none'],
                     help='Way of normalizing the density maps. If fixel, '
                          'will normalize the maps per fixel, \nin each voxel. '
@@ -251,11 +253,9 @@ def main():
             bundles_names.append(Path(bundle).name.split(".")[0])
 
     ### Load the data
-    # The bundles will be loaded in the loop below
     logging.info("Loading data.")
-    peaks_img = nib.load(args.in_peaks)
-    peaks = peaks_img.get_fdata()
-    affine = peaks_img.affine
+    peaks_simg = StatefulImage.load(args.in_peaks, is_orientation=True)
+    peaks = peaks_simg.to_voxel_direction(is_peaks=True)
 
     # Compute NuFo single-fiber from peaks
     if args.single_bundle:
@@ -263,9 +263,18 @@ def main():
         no_second_peak = np.sum(peaks[..., 3:6], axis=-1) == 0
         nufo_sf = np.logical_and(is_first_peak, no_second_peak)
 
+    # Rebind each bundle to the peaks image grid.
+    # bbox_valid_check=False allows streamline points near the volume edge after grid reorientation.
+    sfts = []
+    for bundle in args.in_bundles:
+        sft = load_tractogram(bundle, 'same', bbox_valid_check=False)
+        sft = rebind_sft_to_simg(sft, peaks_simg)
+        sft.remove_invalid_streamlines()
+        sfts.append(sft)
+
     # Compute fixel density (FD) maps and masks
     logging.info("Computing fixel density for all bundles.")
-    fd_maps_original = fixel_density(peaks, args.in_bundles, args.dps_key,
+    fd_maps_original = fixel_density(peaks, sfts, args.dps_key,
                                      args.max_theta,
                                      nbr_processes=args.nbr_processes)
 
@@ -298,70 +307,91 @@ def main():
             # Making sure we also have single-fiber voxels only
             one_bundle_per_voxel *= nufo_sf
             # Save a single-fiber single-bundle mask for the whole WM
-            nib.save(
-                nib.Nifti1Image(one_bundle_per_voxel.astype(np.uint8), affine),
+            StatefulImage.create_from(
+                one_bundle_per_voxel.astype(np.uint8), peaks_simg,
+                is_orientation=False).save(
                 "{}_{}_WM{}".format(sb_mask_name, norm_name, suffix))
 
         for i, bundle_n in enumerate(bundles_names):
             bundle_suffix = '{}_{}{}'.format(norm_name, bundle_n, suffix)
 
             if args.split_bundles:  # Save the maps and masks for each bundle
-                nib.save(nib.Nifti1Image(fd_maps[..., i], affine),
-                         "{}_{}".format(fd_map_name, bundle_suffix))
-                nib.save(nib.Nifti1Image(fd_masks[..., i], affine),
-                         "{}_{}".format(fd_mask_name, bundle_suffix))
+                StatefulImage.create_from(
+                    fd_maps[..., i], peaks_simg, is_orientation=False).save(
+                    "{}_{}".format(fd_map_name, bundle_suffix))
+                StatefulImage.create_from(
+                    fd_masks[..., i], peaks_simg, is_orientation=False).save(
+                    "{}_{}".format(fd_mask_name, bundle_suffix))
                 if norm != "fixel":  # If fixel, voxel maps mean nothing
-                    nib.save(nib.Nifti1Image(vd_maps[..., i], affine),
-                             "{}_{}".format(vd_map_name, bundle_suffix))
+                    StatefulImage.create_from(
+                        vd_maps[..., i], peaks_simg,
+                        is_orientation=False).save(
+                        "{}_{}".format(vd_map_name, bundle_suffix))
                 bundle_mask = vd_masks[..., i].astype(np.uint8)
-                nib.save(nib.Nifti1Image(bundle_mask, affine),
-                         "{}_{}".format(vd_mask_name, bundle_suffix))
+                StatefulImage.create_from(
+                    bundle_mask, peaks_simg, is_orientation=False).save(
+                    "{}_{}".format(vd_mask_name, bundle_suffix))
 
             if args.single_bundle:
                 # Save a single-fiber single-bundle mask for each bundle
                 bundle_mask = fd_masks[..., 0, i] * one_bundle_per_voxel
-                nib.save(nib.Nifti1Image(bundle_mask.astype(np.uint8), affine),
-                         "{}_{}".format(sb_mask_name, bundle_suffix))
+                StatefulImage.create_from(
+                    bundle_mask.astype(np.uint8), peaks_simg,
+                    is_orientation=False).save(
+                    "{}_{}".format(sb_mask_name, bundle_suffix))
 
         if args.split_fixels:  # Save the maps and masks for each fixel
             for i in range(5):
                 fixel_suffix = '{}_f{}{}'.format(norm_name, i + 1, suffix)
 
-                nib.save(nib.Nifti1Image(fd_maps[..., i, :], affine),
-                         "{}_{}".format(fd_map_name, fixel_suffix))
-                nib.save(nib.Nifti1Image(fd_masks[..., i, :], affine),
-                         "{}_{}".format(fd_mask_name, fixel_suffix))
+                StatefulImage.create_from(
+                    fd_maps[..., i, :], peaks_simg,
+                    is_orientation=False).save(
+                    "{}_{}".format(fd_map_name, fixel_suffix))
+                StatefulImage.create_from(
+                    fd_masks[..., i, :], peaks_simg,
+                    is_orientation=False).save(
+                    "{}_{}".format(fd_mask_name, fixel_suffix))
                 if norm != "fixel":  # If fixel, voxel maps mean nothing
-                    nib.save(nib.Nifti1Image(vd_maps[..., i, :], affine),
-                             "{}_{}".format(vd_map_name, fixel_suffix))
+                    StatefulImage.create_from(
+                        vd_maps[..., i, :], peaks_simg,
+                        is_orientation=False).save(
+                        "{}_{}".format(vd_map_name, fixel_suffix))
                 bundle_mask = vd_masks[..., i, :].astype(np.uint8)
-                nib.save(nib.Nifti1Image(bundle_mask, affine),
-                         "{}_{}".format(vd_mask_name, fixel_suffix))
+                StatefulImage.create_from(
+                    bundle_mask, peaks_simg, is_orientation=False).save(
+                    "{}_{}".format(vd_mask_name, fixel_suffix))
 
         norm_suffix = '{}{}'.format(norm_name, suffix)
 
         # Save full fixel density maps, all fixels and bundles combined
-        nib.save(nib.Nifti1Image(fd_maps, affine),
-                 "{}s_{}".format(fd_map_name, norm_suffix))
+        StatefulImage.create_from(
+            fd_maps, peaks_simg, is_orientation=False).save(
+            "{}s_{}".format(fd_map_name, norm_suffix))
 
         # Save full fixel density masks, all fixels and bundles combined
-        nib.save(nib.Nifti1Image(fd_masks, affine),
-                 "{}s_{}".format(fd_mask_name, norm_suffix))
+        StatefulImage.create_from(
+            fd_masks, peaks_simg, is_orientation=False).save(
+            "{}s_{}".format(fd_mask_name, norm_suffix))
 
         # Save full voxel density maps and masks
         if norm != "fixel":  # If fixel, voxel maps mean nothing
-            nib.save(nib.Nifti1Image(vd_maps, affine),
-                     "{}s_{}".format(vd_map_name, norm_suffix))
-        nib.save(nib.Nifti1Image(vd_masks.astype(np.uint8), affine),
-                 "{}s_{}".format(vd_mask_name, norm_suffix))
+            StatefulImage.create_from(
+                vd_maps, peaks_simg, is_orientation=False).save(
+                "{}s_{}".format(vd_map_name, norm_suffix))
+        StatefulImage.create_from(
+            vd_masks.astype(np.uint8), peaks_simg, is_orientation=False).save(
+            "{}s_{}".format(vd_mask_name, norm_suffix))
 
         # Save number of bundles per fixel and per voxel
-        nib.save(nib.Nifti1Image(nb_bundles_per_fixel.astype(np.uint8),
-                                 affine),
-                 "{}_per_fixel_{}".format(nb_bundles_name, norm_suffix))
-        nib.save(nib.Nifti1Image(nb_bundles_per_voxel.astype(np.uint8),
-                                 affine),
-                 "{}_per_voxel_{}".format(nb_bundles_name, norm_suffix))
+        StatefulImage.create_from(
+            nb_bundles_per_fixel.astype(np.uint8), peaks_simg,
+            is_orientation=False).save(
+            "{}_per_fixel_{}".format(nb_bundles_name, norm_suffix))
+        StatefulImage.create_from(
+            nb_bundles_per_voxel.astype(np.uint8), peaks_simg,
+            is_orientation=False).save(
+            "{}_per_voxel_{}".format(nb_bundles_name, norm_suffix))
 
     # Save bundles lookup table to know the order of the bundles
     bundles_idx = np.arange(0, len(bundles_names), 1)

src/scilpy/cli/scil_bundle_mean_fixel_mrds_metric.py
@@ -27,10 +27,11 @@ Please use a bundle file rather than a whole tractogram.
 
 import argparse
 
-import nibabel as nib
 import numpy as np
 
-from scilpy.io.streamlines import load_tractogram_with_reference
+from scilpy.io.stateful_image import StatefulImage
+from scilpy.io.streamlines import (load_tractogram_with_reference,
+                                   rebind_sft_to_simg)
 from scilpy.io.utils import (add_overwrite_arg,
                              add_reference_arg,
                              assert_headers_compatible,
@@ -105,24 +106,34 @@ def main():
 
     assert_outputs_exist(parser, args, out_metrics)
 
-    sft = load_tractogram_with_reference(parser, args, args.in_bundle)
-    pdds_img = nib.load(args.in_pdds)
-    affine = pdds_img.affine
-    header = pdds_img.header
+    # PDDs are directional (peaks-like): reorient the grid to RAS and
+    # convert the direction vectors from world to voxel space.
+    pdds_simg = StatefulImage.load(args.in_pdds, is_orientation=True)
+    pdds_simg.to_ras()
+    pdds_data = pdds_simg.to_voxel_direction(is_peaks=True)
 
-    in_metrics_data = [nib.load(metric).get_fdata(dtype=np.float32) for metric in in_metrics]
+    # The per-fixel metrics (FA/MD/RD/AD) are scalar, just need the
+    # matching grid.
+    in_metrics_data = []
+    for metric in in_metrics:
+        metric_simg = StatefulImage.load(metric)
+        metric_simg.to_ras()
+        in_metrics_data.append(metric_simg.get_fdata(dtype=np.float32))
+
+    sft = load_tractogram_with_reference(parser, args, args.in_bundle)
+    sft = rebind_sft_to_simg(sft, pdds_simg)
+
     fixel_metrics =\
         mrds_metrics_along_streamlines(sft,
-                                       pdds_img.get_fdata(dtype=np.float32),
+                                       pdds_data,
                                        in_metrics_data,
                                        args.max_theta,
                                        args.length_weighting)
 
     for metric_id, curr_metric in enumerate(fixel_metrics):
-        nib.Nifti1Image(curr_metric.astype(np.float32),
-                        affine=affine,
-                        header=header,
-                        dtype=np.float32).to_filename(out_metrics[metric_id])
+        StatefulImage.create_from(
+            curr_metric.astype(np.float32), pdds_simg).save(
+            out_metrics[metric_id])
 
 
 if __name__ == '__main__':

src/scilpy/cli/scil_tractogram_compute_ae.py
@@ -29,10 +29,11 @@ Hints:
 import argparse
 import logging
 
-import nibabel as nib
 import numpy as np
 
-from scilpy.io.streamlines import (load_tractogram_with_reference, 
+from scilpy.io.stateful_image import StatefulImage
+from scilpy.io.streamlines import (load_tractogram_with_reference,
+                                   rebind_sft_to_simg,
                                    save_tractogram)
 from scilpy.io.utils import (add_processes_arg, add_verbose_arg, 
                              add_overwrite_arg, assert_headers_compatible, 
@@ -107,8 +108,10 @@ def main():
     cmap = get_lookup_table(args.cmap)
 
     # -- Loading
-    peaks = nib.load(args.in_peaks).get_fdata()
+    peaks_simg = StatefulImage.load(args.in_peaks, is_orientation=True)
+    peaks = peaks_simg.to_voxel_direction(is_peaks=True)
     sft = load_tractogram_with_reference(parser, args, args.in_tractogram)
+    sft = rebind_sft_to_simg(sft, peaks_simg)
     logging.info("Loaded data")
 
     # Removing invalid
@@ -131,11 +134,18 @@ def main():
     ae = compute_ae(sft, peaks, nb_processes=args.nbr_processes)
 
     # Printing stats
-    stacked_ae = np.hstack(ae)
-    mean_ae = np.mean(stacked_ae)
-    std_ae = np.std(stacked_ae)
-    min_ae = np.min(stacked_ae)
-    max_ae = np.max(stacked_ae)
+    if len(ae) == 0:
+        stacked_ae = np.array([])
+        mean_ae = np.nan
+        std_ae = np.nan
+        min_ae = np.nan
+        max_ae = np.nan
+    else:
+        stacked_ae = np.hstack(ae)
+        mean_ae = np.mean(stacked_ae)
+        std_ae = np.std(stacked_ae)
+        min_ae = np.min(stacked_ae)
+        max_ae = np.max(stacked_ae)
     logging.info("AE computed. Some statistics:\n"
                  "- Mean AE: {} +- {} \n"
                  "- Range:[{}, {}]".format(mean_ae, std_ae, min_ae, max_ae))
@@ -146,8 +156,11 @@ def main():
 
     # Add as color (optional)
     if args.save_as_color:
-        max_cmap = args.cmap_max if args.cmap_max is not None \
-            else np.max(stacked_ae)
+        if len(stacked_ae) == 0:
+            max_cmap = 180
+        else:
+            max_cmap = args.cmap_max if args.cmap_max is not None \
+                else np.max(stacked_ae)
         logging.info("Saving colors. The maxium color is assiociated to "
                      "value {}".format(max_cmap))
          
@@ -164,7 +177,8 @@ def main():
         the_map = project_dpp_to_map(sft, args.dpp_key)
 
         logging.info("Saving file {}".format(args.save_mean_map))
-        nib.save(nib.Nifti1Image(the_map, sft.affine), args.save_mean_map)
+        StatefulImage.create_from(
+            the_map, peaks_simg, is_orientation=False).save(args.save_mean_map)
 
 
 if __name__ == "__main__":

src/scilpy/cli/tests/test_bundle_fixel_analysis.py
@@ -4,8 +4,13 @@
 import os
 import tempfile
 
+import nibabel as nib
+import numpy as np
+from dipy.io.stateful_tractogram import Space, StatefulTractogram
+
 from scilpy import SCILPY_HOME
 from scilpy.io.fetcher import fetch_data, get_testing_files_dict
+from scilpy.io.streamlines import save_tractogram
 
 fetch_data(get_testing_files_dict(), keys=['commit_amico.zip'])
 tmp_dir = tempfile.TemporaryDirectory()
@@ -26,6 +31,12 @@ def test_default_parameters(script_runner, monkeypatch):
                              '--in_bundles', in_bundle,
                              '--processes', '4', '-f'])
     assert ret.success
+    fd_name = os.path.join('fixel_analysis',
+                           'fixel_density_maps_voxel-norm.nii.gz')
+    v_name = os.path.join('fixel_analysis',
+                          'fixel_density_maps_v-norm.nii.gz')
+    assert os.path.isfile(fd_name)
+    assert not os.path.isfile(v_name)
 
 
 def test_all_parameters(script_runner, monkeypatch):
@@ -82,5 +93,97 @@ def test_multiple_norm(script_runner, monkeypatch):
     assert not os.path.isfile('voxel_density_maps_fixel-norm.nii.gz')
     assert os.path.isfile('voxel_density_maps_none-norm.nii.gz')
 
-# We would need a tractogram with data_per_streamline to test the --dps_key
-# option
+
+def test_non_ras_bundle_fixel_analysis(script_runner, tmp_path):
+    # Non-RAS affine (LAS orientation with offset)
+    affine = np.array([
+        [-2.0, 0.0, 0.0, 30.0],
+        [0.0, 2.0, 0.0, 10.0],
+        [0.0, 0.0, 2.0, 5.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ])
+    # Peak at voxel (2, 3, 4) pointing in world X
+    peaks_data = np.zeros((8, 8, 8, 15), dtype=np.float32)
+    peaks_data[2, 3, 4, 0:3] = [1.0, 0.0, 0.0]
+
+    peaks_path = str(tmp_path / "peaks_las.nii.gz")
+    peaks_img = nib.Nifti1Image(peaks_data, affine)
+    nib.save(peaks_img, peaks_path)
+
+    # Streamline through voxel (2, 3, 4) in world space
+    vox_center = np.array([2.0, 3.0, 4.0, 1.0])
+    world_center = affine @ vox_center
+    streamline = np.array([
+        world_center[:3] - np.array([0.4, 0.0, 0.0]),
+        world_center[:3] + np.array([0.4, 0.0, 0.0])
+    ])
+    sft = StatefulTractogram([streamline], peaks_img, Space.RASMM)
+    bundle_path = str(tmp_path / "bundle.trk")
+    save_tractogram(sft, bundle_path, False)
+
+    out_dir = str(tmp_path / "out")
+    ret = script_runner.run(['scil_bundle_fixel_analysis', peaks_path,
+                             '--in_bundles', bundle_path,
+                             '--out_dir', out_dir,
+                             '--processes', '1', '-f'])
+    assert ret.success
+    out_map_path = os.path.join(out_dir,
+                                "fixel_density_maps_voxel-norm.nii.gz")
+    assert os.path.isfile(out_map_path)
+    res_img = nib.load(out_map_path)
+    res_data = res_img.get_fdata()
+    # Fixel 0 at voxel (2, 3, 4) must have density > 0
+    assert res_data[2, 3, 4, 0, 0] > 0.0
+
+
+def test_empty_bundle_fixel_analysis(script_runner, tmp_path):
+    affine = np.diag([2.0, 2.0, 2.0, 1.0])
+    peaks_data = np.zeros((8, 8, 8, 15), dtype=np.float32)
+    peaks_path = str(tmp_path / "peaks.nii.gz")
+    peaks_img = nib.Nifti1Image(peaks_data, affine)
+    nib.save(peaks_img, peaks_path)
+
+    sft = StatefulTractogram([], peaks_img, Space.VOX)
+    bundle_path = str(tmp_path / "empty_bundle.trk")
+    save_tractogram(sft, bundle_path, False)
+
+    out_dir = str(tmp_path / "out_empty")
+    ret = script_runner.run(['scil_bundle_fixel_analysis', peaks_path,
+                             '--in_bundles', bundle_path,
+                             '--out_dir', out_dir,
+                             '--processes', '1', '-f'])
+    assert ret.success
+    out_map_path = os.path.join(out_dir,
+                                "fixel_density_maps_voxel-norm.nii.gz")
+    assert os.path.isfile(out_map_path)
+    res_data = nib.load(out_map_path).get_fdata()
+    assert np.all(res_data == 0.0)
+
+
+def test_out_of_bounds_bundle_fixel_analysis(script_runner, tmp_path):
+    affine = np.diag([2.0, 2.0, 2.0, 1.0])
+    peaks_data = np.zeros((8, 8, 8, 15), dtype=np.float32)
+    peaks_path = str(tmp_path / "peaks_oob.nii.gz")
+    peaks_img = nib.Nifti1Image(peaks_data, affine)
+    nib.save(peaks_img, peaks_path)
+
+    # Streamline completely outside bounding box
+    streamline = np.array([
+        [100.0, 100.0, 100.0],
+        [105.0, 105.0, 105.0]
+    ])
+    sft = StatefulTractogram([streamline], peaks_img, Space.VOX)
+    bundle_path = str(tmp_path / "oob_bundle.trk")
+    save_tractogram(sft, bundle_path, False, bbox_valid_check=False)
+
+    out_dir = str(tmp_path / "out_oob")
+    ret = script_runner.run(['scil_bundle_fixel_analysis', peaks_path,
+                             '--in_bundles', bundle_path,
+                             '--out_dir', out_dir,
+                             '--processes', '1', '-f'])
+    assert ret.success
+    out_map_path = os.path.join(out_dir,
+                                "fixel_density_maps_voxel-norm.nii.gz")
+    assert os.path.isfile(out_map_path)
+    res_data = nib.load(out_map_path).get_fdata()
+    assert np.all(res_data == 0.0)

src/scilpy/tractanalysis/afd_along_streamlines.py
@@ -5,6 +5,7 @@ from dipy.reconst.shm import sh_to_sf_matrix, sph_harm_ind_list
 import numpy as np
 from scipy.special import legendre_p_all
 
+from scilpy.io.stateful_image import StatefulImage
 from scilpy.reconst.utils import find_order_from_nb_coeff
 from scilpy.tractanalysis.voxel_boundary_intersection import\
     subdivide_streamlines_at_voxel_faces
@@ -88,6 +89,11 @@ def _afd_and_rd_sums_along_streamlines(sft, fodf, fodf_basis,
     sft.to_vox()
     sft.to_corner()
 
+    # SH coefficients are evaluated against sphere.vertices, which are in
+    # world orientation. Streamline segments are in voxel space, so bring
+    # them to world orientation before matching against the sphere.
+    to_world_rotation = StatefulImage._get_rotation_matrix(fodf.affine)
+
     fodf_data = fodf.get_fdata(dtype=np.float32)
     order = find_order_from_nb_coeff(fodf_data)
     sphere = get_sphere(name='repulsion724')
@@ -117,8 +123,10 @@ def _afd_and_rd_sums_along_streamlines(sft, fodf, fodf_basis,
         segments = segments[non_zero_lengths]
         seg_lengths = seg_lengths[non_zero_lengths]
 
-        # Find closest point on sphere
-        test = np.dot(segments, sphere.vertices.T)
+        # Find closest point on sphere. segments are in voxel space, rotate
+        # to world orientation to compare against sphere.vertices.
+        world_segments = np.dot(segments, to_world_rotation.T)
+        test = np.dot(world_segments, sphere.vertices.T)
         test2 = (test.T / (seg_lengths * sphere_norm)).T
         angles = np.arccos(test2)
         sorted_angles = np.argsort(angles, axis=1)